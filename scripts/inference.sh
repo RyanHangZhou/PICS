3af8bfce0c76f82808284b30821ae825a601f51b
Hang Zhou <ryanhangzhou@gmail.com>
2026-02-05 11:11:05 -0700

    clean code

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -3,3 +3,7 @@ CUDA_VISIBLE_DEVICES=2 python run_test_wild.py \
     --output "results/LVIS_again3" \
     --obj_thr 2
 
+CUDA_VISIBLE_DEVICES=2 python run_test_wild.py \
+    --input "/data/hang/customization/data/dreambooth_reorganized" \
+    --output "results/dreambooth" \
+    --obj_thr 2
