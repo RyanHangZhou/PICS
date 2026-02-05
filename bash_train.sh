7f50020c176bb45d1011bec819405a2650c7467c
Hang Zhou <ryanhangzhou@gmail.com>
2026-02-05 11:15:02 -0700

    clean code

--- bash_train.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 python run_train_grabcompose.py 
-CUDA_VISIBLE_DEVICES=1,3 python run_train_grabcompose.py 
-
-python
--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=2 python run_test_wild.py \
+CUDA_VISIBLE_DEVICES=2 python run_test.py \
     --input "/data/hang/customization/data/Wild" \
     --output "results/LVIS_again3" \
     --obj_thr 2
 
-CUDA_VISIBLE_DEVICES=2 python run_test_wild.py \
+CUDA_VISIBLE_DEVICES=2 python run_test.py \
     --input "/data/hang/customization/data/dreambooth_reorganized" \
     --output "results/dreambooth" \
     --obj_thr 2
